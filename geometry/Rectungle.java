a6c7c46db277f1424e20aae6e9e6ed29cbe41271
Anastasia R <anastasiarudakova@Anastasias-MacBook-Pro.local>
2024-04-07 16:08:16 +0200

    czesc 5 zadania

---
+++ geometry/Rectungle.java
@@ -0,0 +1,14 @@
+package geometry;
+
+public class Rectangle {
+    
+}
+public class Rectungle {
+    private double lenth;
+    private double with;
+
+public Rectungle(double lenth, double with) {
+    this.length = length;
+    this.width = width; 
+}
+}
