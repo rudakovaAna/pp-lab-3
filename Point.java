745af5617c5ae4e40dfef8f84830a10bf2815896
Anastasia R <anastasiarudakova@Anastasias-MacBook-Pro.local>
2024-04-07 15:20:03 +0200

    zcv

---
+++ Point.java
@@ -0,0 +1,23 @@
+public class Point {
+    private double x;
+    private double y;
+    public double getX() {
+        return this.x;
+
+    }
+    public double getY() {
+        return this.y;
+    }
+    public Point setX(double x) {
+        this.x = x;
+    }
+    public Point setY(double y) {
+        this.y = y;
+        return this;
+    
+    }
+    public Point(double x, double y) {
+        this.y=y;
+        this.x=x;
+    } 
+}
