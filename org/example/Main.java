cbc4a718d59fe13fd49cef20a9766a0407c5d964
Anastasia R <anastasiarudakova@Anastasias-MacBook-Pro.local>
2024-04-07 15:59:50 +0200

    vbg

--- org/example/Main.java
+++ org/example/Main.java
@@ -23,3 +23,49 @@ public class Main {
         cc.getColor()
         };
 }
+Circle[] circles = new Circle[10];
+for (int index = 0; index < 10; index++) {
+    circles[index] = new Circle(
+    new Point(Math.random() * 10, Math.random() * 10),
+    Math.random() * 5);
+}
+for (int circleIndex = 0; circleIndex < 10; circleIndex++) {
+    circles[circleIndex].calculateArea();
+}
+
+ColoredCircle[] coloredCircles = new ColoredCircle[10];
+for (int index = 0; index < 10; index++) {
+coloredCircles[index] = new ColoredCircle(
+new Point(Math.random() * 10, Math.random() * 10),
+Math.random() * 5, getRandomColor());
+
+    }
+    for (int coloredCircleIndex = 0; coloredCircleIndex < 10; coloredCircleIndex++) {
+        var coloredCircle = coloredCircles[coloredCircleIndex];
+        coloredCircle.calculateArea();
+        System.out.printf("Circle color: %s", coloredCircle.getColor());
+    }
+}
+private statistics Color getRandomColor() {
+    Random rand = new Rndom();
+    Color color;
+    switch (rand.nextInt(1, 3)) {
+        case 1: {
+            color = Color.red;
+            break;
+        }
+        case 3; {
+            color = Color.black;
+            break;
+
+        }
+        default: {
+            color = Color.GREEN;
+            break;
+        }
+    }
+    return color;
+}
+    }
+
+
